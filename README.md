c708df74a4cc532ac35afceda3fbe61c71842929
repat <repat@repat.de>
2014-01-07 01:17:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+Minimal Open Source Flashlight(Older SDKs)
+======
+**Minimal Open Source Flashlight** a minimalistic approach to flashlights on Android. It consists of only one switch to turn the camera LED on and off. Not more. No widget. No timer. No colored display. No unnecessary artwork.
+
+Check out the [version for Android 4.0+](http://github.com/repat/mosf).
+
+#### Screenshot
+![MOSF Screenshot](http://repat.de/Bilder/mosf-older-sdks-screenshot.png "MOSF Screenshot")
+
+#### Google Play Store
+[![Get it on Google Play](https://developer.android.com/images/brand/en_generic_rgb_wo_45.png)](https://play.google.com/store/apps/details?id=de.repat.mosf)
+
+#### Market Link
+![QR-Code](http://repat.de/Bilder/qrcode-mosf.png)
+
+## Tested on
+#### Works on
+* HTC Desire
+* Motorola Moto G
+* Sony Xperia Acro S
+* Samsung Galaxy SIII Mini (GT-I8190)
+* HTC Sensation
+* HTC Desire S
+* Samsung Galaxy Note 10.1
+* Samsung Galaxy SII (GT-I9100)
+* Samsung Galaxy S4 (GT-I9505)
+* Samsung Galaxy S4 mini
+
+#### Does not work on
+* Galaxy Nexus i925/ Nexus Prime
+
+## Artwork
+* Icon: http://openclipart.org/detail/1638/flashlight-by-johnny_automatic
+
+## Permissions
+* CAMERA, because the LED is internally controlled by the camera object.
+
+## License 
+* [MIT](http://opensource.org/licenses/MIT)
+
+## Contact
+#### repat
+* http://repat.de
+* email: repat[at]repat[dot]de
+* XMPP: repat@jabber.ccc.de
+* Twitter: [@repat123](https://twitter.com/repat123 "repat123 on twitter")
+
+[![Flattr this git repo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=repat&url=https://github.com/repat/mosf&title=mosf&language=&tags=github&category=software) 
+
---
+++ src/de/repat/mosf/Flashlight.java
@@ -0,0 +1,65 @@
+package de.repat.mosf;
+
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ToggleButton;
+import android.app.Activity;
+
+public class Flashlight extends Activity {
+    private Camera cam;
+    private Parameters p;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // delete title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // start GUI
+        setContentView(R.layout.activity_flashlight);
+    }
+
+    public void onToggleLEDClicked(View v) {
+        // Button pressed
+        boolean isChecked = ((ToggleButton) v).isChecked();
+
+        // turn off
+        if (!isChecked) {
+            if (cam != null) {
+                p.setFlashMode(Parameters.FLASH_MODE_OFF);
+                cam.setParameters(p);
+                cam.stopPreview();
+                cam.setPreviewCallback(null);
+                cam.release();
+                cam = null;
+            }
+
+        }
+        // turn on
+        else {
+            if (cam == null) {
+                try {
+                    cam = Camera.open();
+                    // Yeah, this could be more specific maybe.
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                    finish();
+                    return;
+                }
+                p = cam.getParameters();
+                p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                cam.setParameters(p);
+                cam.startPreview();
+            }
+        }
+    }
+
+}
